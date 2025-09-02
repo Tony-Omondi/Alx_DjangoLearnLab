778bdfd689b70cabe787d7e43e7f2dd532560c5c
Tony-Omondi <otienotony598@gmail.com>
2025-09-02 13:42:53 +0300

    finally

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,25 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
 
-# Function-based view to list all books
+# Function-based view: list all books
 def list_books(request):
-    try:
-        books = Book.objects.all()
-        return render(request, 'relationship_app/list_books.html', {'books': books})
-    except Exception as e:
-        return render(request, 'relationship_app/error.html', {'error': str(e)})
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
-# Class-based view to display library details
+# Class-based view: show details for a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        try:
-            context['books'] = self.object.books.all()
-        except Exception as e:
-            context['error'] = f"Error retrieving books: {str(e)}"
-        return context
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
