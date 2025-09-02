59d4fab57bb9216dc7c12dfe2fd2b2f5e28c4933
Tony-Omondi <otienotony598@gmail.com>
2025-09-02 14:11:17 +0300

    finally

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.views.generic.detail import DetailView
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import  permission_required, login_required, user_passes_test
 from .models import Library, Book, UserProfile
 
 # Function-based view for listing books
