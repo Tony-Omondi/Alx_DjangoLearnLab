6cd52de3925e5c527d332b1f436b99a0e0f12ca6
Tony-Omondi <otienotony598@gmail.com>
2025-09-11 16:31:09 +0300

    abstract user

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -9,12 +9,29 @@ class Author(models.Model):
         return self.name
 
 
+from django.db import models
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+            ("can_add_book", "Can add a new book"),
+            ("can_change_book", "Can change an existing book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
 
     def __str__(self):
-        return self.title
+        return f"{self.title} by {self.author} ({self.publication_year})"
+
+
+
 
 
 class Library(models.Model):
