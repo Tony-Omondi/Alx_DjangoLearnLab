f637012a7c5bfd6c54aa49fb0b807c31c83c10f5
Tony-Omondi <otienotony598@gmail.com>
2025-09-11 16:20:13 +0300

    abstract user

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,46 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, Author, Library, Librarian, CustomUser
+from django.utils.translation import gettext_lazy as _
 
+# Register your existing models
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "publication_year")  # columns to display
-    list_filter = ("publication_year", "author")            # filters sidebar
-    search_fields = ("title", "author")                     # search box
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    filter_horizontal = ('books',)
+
+@admin.register(Librarian)
+class LibrarianAdmin(admin.ModelAdmin):
+    list_display = ('name', 'library')
+
+# Custom User Admin
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'first_name', 'last_name', 'date_of_birth', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'date_of_birth', 'profile_photo', 'password1', 'password2', 'is_staff', 'is_superuser')}
+        ),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+# Register CustomUser
+admin.site.register(CustomUser, CustomUserAdmin)
