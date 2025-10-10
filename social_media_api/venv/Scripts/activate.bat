2d539767cd48fdd4a7323cc89e87e7cdc0e5dfe4
Tony-Omondi <otienotony598@gmail.com>
2025-10-10 16:35:17 +0300

    serializer

--- social_media_api/venv/Scripts/activate.bat
+++ social_media_api/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Desktop\projeects\Alx_DjangoLearnLab\social_media_api\venv
+set VIRTUAL_ENV=C:\Users\user\Desktop\Alx_DjangoLearnLab\social_media_api\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
