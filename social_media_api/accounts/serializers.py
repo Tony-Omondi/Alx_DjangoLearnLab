7e834f9603500b3944d87f0e03a8dc39c6c09fac
Tony-Omondi <otienotony598@gmail.com>
2025-10-10 16:22:34 +0300

    firsr commit

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import CustomUser
+from rest_framework.authtoken.models import Token
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+        read_only_fields = ['followers']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+        )
+        Token.objects.create(user=user)
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
